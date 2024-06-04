058339f5beda90bf6f03a01af8e826e3582a061b
GlynDobson <glynd@cloudsailer.net>
2024-06-04 17:37:38 -0400

    Create oAuthBodyTemplate.ps1

---
+++ API/UEM/oAuthBodyTemplate.ps1
@@ -0,0 +1,40 @@
+$client_id = "xxx" 
+$client_secret = "xxx"
+
+$acces_token_url = "https://na.uemauth.vmwservices.com/connect/token"
+
+try
+{
+
+$oAuthbody = @{ 
+    grant_type = "client_credentials" 
+    client_id = $client_id 
+    client_secret = $client_secret 
+}
+
+   $csvUserList = 103517
+
+   $response = Invoke-WebRequest -Method Post -Uri $acces_token_url -Body $oAuthbody 
+   $oAuthToken = $response | ConvertFrom-Json 
+   $oAuthToken = $oAuthToken.access_token
+
+   $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+   $headers.Add("Authorization", "Bearer $oAuthToken")
+   $headers.Add("Accept", 'application/json')
+   $headers.Add("content-type", 'application/json')
+
+   $Body = "{`"BulkValues`":{`"Value`":[" + $csvUserList + "]}}"
+
+   $URI = "https://as135.awmdm.com/api/mdm/devices?searchby=DeviceId"
+   
+   $response = Invoke-WebRequest -Uri $URI -Headers $headers -Method POST -Body $Body
+
+   ($response.Content | ConvertFrom-Json).devices
+
+}
+catch
+{
+   Write-Host "StatusCode: $_.Exception.Response.StatusCode.value__ "
+   Write-Host "StatusDescription: $_.Exception.Response.StatusDescription"
+   Write-Host $_.Exception.message
+}
